7003c6b60bf5f0f088ea893a5defc9db1a6f37f5
Kyle Lape <kyle.lape@redhat.com>
2014-05-23 14:30:14 -0700

    Adding code to get the original URI in the endpoint

--- WEB-INF/classes/com/redhat/gss/HelloEndpoint.java
+++ WEB-INF/classes/com/redhat/gss/HelloEndpoint.java
@@ -1,10 +1,25 @@
 package com.redhat.gss;
 
 import javax.jws.WebService;
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceContext;
+import org.jboss.logging.Logger;
+import javax.xml.ws.handler.MessageContext;
+import javax.servlet.ServletRequest;
 
 @WebService
 public class HelloEndpoint {
+  private static Logger log = Logger.getLogger(HelloEndpoint.class);
+
+  @Resource
+  WebServiceContext wsContext;
+
   public String sayHello(String name) {
+    ServletRequest servletRequest = (ServletRequest)wsContext.getMessageContext().get(MessageContext.SERVLET_REQUEST);
+    String originalUri = servletRequest.getParameter("originalUri");
+    if(originalUri != null) {
+      log.info("Original URI: " + originalUri);
+    }
     return "Hello, " + name;
   }
 }
