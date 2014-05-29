ddbff21062c08da8116947be602492d4d0b2daca
Kyle Lape <kyle.lape@redhat.com>
2014-05-29 11:28:01 -0700

    Adding CLI command to install valve

--- installModuleZip.sh
+++ installModuleZip.sh
@@ -6,3 +6,4 @@ if [ ! "$JBOSS_HOME" ]; then
 fi
 
 unzip -d "$JBOSS_HOME" valveModule.zip
+$JBOSS_HOME/bin/jboss-cli.sh -c --command='/subsystem=web/valve=multipleUri:add(module="com.redhat.gss.valve",class-name="com.redhat.gss.valve.UriValve")'
