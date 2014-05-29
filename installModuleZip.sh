97c0b651b31699e8080a4bd64d310e6435f118fb
Kyle Lape <kyle.lape@redhat.com>
2014-05-29 11:35:50 -0700

    Combining the create and install scripts

--- installModuleZip.sh
+++ installModuleZip.sh
@@ -5,5 +5,25 @@ if [ ! "$JBOSS_HOME" ]; then
   exit 1
 fi
 
+mvn clean install
+
+if [ $? -gt 0 ]; then
+  echo
+  echo ------------------------------------------
+  echo Maven build failed.  Not installing module.
+  echo ------------------------------------------
+  echo
+  exit 1
+fi 
+
+moduleDir="modules/system/layers/base/com/redhat/gss/valve/main"
+
+mkdir -p $moduleDir
+
+cp valve/target/multipleUriValve.jar $moduleDir
+cp valve/src/main/resources/module.xml $moduleDir
+zip -r valveModule.zip modules/ > /dev/null
+rm -rf modules/
+
 unzip -d "$JBOSS_HOME" valveModule.zip
 $JBOSS_HOME/bin/jboss-cli.sh -c --command='/subsystem=web/valve=multipleUri:add(module="com.redhat.gss.valve",class-name="com.redhat.gss.valve.UriValve")'
